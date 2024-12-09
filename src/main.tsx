d61b8c9c9087e71928a746bc69b355eddb060553
vishnu5104 <vishnus510412@gmail.com>
2024-12-09 13:16:55 +0530

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Studio from './routes/Studio';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Studio /> },
+  { path: '/studio', element: <App /> },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
