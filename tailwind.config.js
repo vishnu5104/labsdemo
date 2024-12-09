b5571d16541d006ea847961f8672c80ced99169b
vishnu5104 <vishnus510412@gmail.com>
2024-12-09 13:25:14 +0530

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import { themes } from 'daisyui/src/theming/themes';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
